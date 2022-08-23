a8dd7988cd91c74142c573b0b52919069d6f1d6d
Ephie Adhiambo <ephie.adhiambo@student.moringaschool.com>
2022-08-23 14:58:49 +0300

    changes

---
+++ app.js
@@ -0,0 +1,123 @@
+$(document).ready(function () {
+    $("#axela-image").click(function () {
+        $("#axela").slideToggle(1000);
+    });
+    $("#axela").click(function () {
+        $("#axela").slideToggle(1000);
+        
+    });
+  });
+  
+  $(document).ready(function () {
+    $("#vitz-image").click(function () {
+        $("#vitz").slideToggle(1000);
+    });
+    $("#vitz").click(function () {
+        $("#vitz").slideToggle(1000);
+        
+    });
+  });
+  
+  $(document).ready(function () {
+    $("#mazda-image").click(function () {
+        $("#mazda").slideToggle(1000);
+    });
+    $("#mazda").click(function () {
+        $("#mazda").slideToggle(1000);
+        
+    });
+  });
+  
+  $(document).ready(function () {
+    $("#atenza-image").click(function () {
+        $("#atenza").slideToggle(1000);
+    });
+    $("#atenza").click(function () {
+        $("#atenza").slideToggle(1000);
+        
+    });
+  });
+  
+  $(document).ready(function () {
+    $("#cx5-image").click(function () {
+        $("#cx5").slideToggle(1000);
+    });
+    $("#cx5").click(function () {
+        $("#cx5").slideToggle(1000);
+        
+    });
+  });
+  
+  $(document).ready(function () {
+    $("#demio-image").click(function () {
+        $("#demio").slideToggle(1000);
+    });
+    $("#demio").click(function () {
+        $("#demio").slideToggle(1000);
+        
+    });
+  });
+  
+  $(document).ready(function () {
+    $("#subaru-image").click(function () {
+        $("#subaru").slideToggle(1000);
+    });
+    $("#subaru").click(function () {
+        $("#subaru").slideToggle(1000);
+        
+    });
+  });
+  
+  $(document).ready(function () {
+    $("#impreza-image").click(function () {
+        $("#impreza").slideToggle(1000);
+    });
+    $("#impreza").click(function () {
+        $("#impreza").slideToggle(1000);
+        
+    });
+  });
+  
+  
+  $(document).ready(function () {
+    $("#axio-image").click(function () {
+        $("#axio").slideToggle(1000);
+    });
+    $("#axio").click(function () {
+        $("#axio").slideToggle(1000);
+        
+    });
+  });
+  
+  $(document).ready(function () {
+    $("#allion-image").click(function () {
+        $("#allion").slideToggle(1000);
+    });
+    $("#allion").click(function () {
+        $("#allion").slideToggle(1000);
+        
+    });
+  });
+  
+  
+  
+  
+  
+  
+  
+  
+    
+  //  function totalAmount() {
+  //    alert(
+  //      "We have recieved your Message." 
+       
+  //    );
+    
+  //    }if (email== "" || name== "" || message== "") {
+  //     alert("Put in your details below!");
+  //   }
+  
+  
+  
+    // order form
+    
